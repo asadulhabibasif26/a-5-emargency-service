ceebc8ad13ce66bb88a08a65ea4f50da557c610b
asadulhabibasif26 <eloopazbd@gmail.com>
2025-08-27 20:52:18 +0600

    copy button complete

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
+// shared function 
 
+function copyCounter ( number , title) {
+    
+    const copyNumber = document.getElementById(number).innerText
+    const copyNTitle = document.getElementById(title).innerText
+    alert("Copy -" + copyNTitle + "- number - " + copyNumber);
+    const copyContainer = document.getElementById('copy-container')
+    const copyContainerNumber = parseInt(document.getElementById('copy-container').innerText)
 
-// copy count js section 
+    const newCopyContainer = copyContainerNumber + 1 ;
+
+    copyContainer.innerText = newCopyContainer
+}
 
 document.getElementById('ne-copy-btn').addEventListener('click' , function(){
-    console.log('clicked');
+    copyCounter('ne-number' , 'ne-title');
 })
+document.getElementById('police-copy-btn').addEventListener('click' , function(){
+    copyCounter('police-number' , 'police-title');
+})
+document.getElementById('fire-btn').addEventListener('click' , function(){
+    copyCounter('fire-number' , 'fire-title');
+})
+document.getElementById('ambulance-copy-btn').addEventListener('click' , function(){
+    copyCounter('ambulance-number' , 'ambulance-title');
+})
+document.getElementById('w&c-copy-btn').addEventListener('click' , function(){
+    copyCounter('w&c-number' , 'w&c-title');
+})
+document.getElementById('ac-copy-btn').addEventListener('click' , function(){
+    copyCounter('ac-number' , 'ac-title');
+})
+document.getElementById('Electricity-copy-btn').addEventListener('click' , function(){
+    copyCounter( 'Electricity-number' , 'Electricity-title');
+})
+document.getElementById('brac-copy-btn').addEventListener('click' , function(){
+    copyCounter('brac-number' , 'Brac-title');
+})
+document.getElementById('railway-copy-btn').addEventListener('click' , function(){
+    copyCounter('railway-number' , 'railway-title');
+})
+
+
+
+
+
